7b916d475e14215c2fb08b83024080418e951e6b
Nathan Hoekstra <dev@nathanhoekstra.nl>
2023-12-07 21:34:37 +0100

    Modified code to use f strings

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ config['enable_annotation'] = True
 
 
 # ---- Time settings  ----
-config['am'] = 600
-config['pm'] = 1900
-config['delay'] = 120
+config['am'] = 600 # Start time to take pictures
+config['pm'] = 1900 # End time to take pictures
+config['delay'] = 120 # Delay in seconds between pictures
 
 # ---- Raspistill image settings ----
 config['flip_horizontal'] = False
 config['flip_vertical'] = False
-config['width'] = 1920 #Max width is 3280 pixels
-config['height'] = 1080 #Max height is 2464 pixels
-config['quality'] = 35 # Set JPEG quality default is 35, range 0-100
+config['width'] = 3200 # Max width is 3280 pixels
+config['height'] = 1800 # Max height is 2464 pixels
+config['quality'] = 50 # Set JPEG quality default is 35, range 0-100
 
 # ----- Annotation settings
-config['annotation_text'] = '(c) N.Hoekstra'
+config['annotation_text'] = '(C) N.Hoekstra'
--- take.py
+++ take.py
@@ -3,77 +3,74 @@ import time
 from datetime import datetime
 from config import config
 
-def create_log(string):
+def create_log(string: str):
 	i = datetime.now()
 	time = i.strftime('[%d-%m-%Y %H:%M:%S]')
 
 	with open('log.txt', 'a') as log_file:
-		log_file.write(time + ' ' + string + '\n')
+		log_file.write(f'{time} {string}\n')
 
-def try_to_mkdir(path):
+def try_to_mkdir(path: str):
 	if os.path.exists(path) == False:
 		os.makedirs(path)
 
-def prepare_dir(base, now):
+def prepare_dir(base: str, now: datetime):
 	path = now.strftime('%d-%m-%Y')
-	try_to_mkdir(base + '/' + path)
+	try_to_mkdir(f'{base}/{path}')
 	return path
 
-def make_os_command(config, file_name):
-	height = config['height']
-	width = config['width']
+def make_os_command(file_name: str):
+	os_command = f'/usr/bin/raspistill -q {config["quality"]} '
+	if config["flip_horizontal"]:
+		os_command += '-hf '
+	if config["flip_vertical"]:
+		os_command += '-vf '
 
-	os_command = '/usr/bin/raspistill -q ' + str(config['quality']) + ' '
-	if config['flip_horizontal']:
-		os_command = os_command + '-hf '
-	if config['flip_vertical']:
-		os_command = os_command + '-vf '
-
-	os_command = os_command + '-h ' + str(height)+ ' -w ' + str(width) + ' -o ' + file_name
+	os_command += f'-h {config["height"]} -w {config["width"]} -o {file_name}'
 	return os_command
 
-def annotate(file_name):
+def annotate(file_name: str):
 	i = datetime.now()
 	time = i.strftime('%d-%m-%Y %H:%M:%S')
 
-	os_command = '/usr/bin/convert ' + file_name + ' -pointsize 25 -fill white -annotate +1680+1060 ' + repr(time) + ' '
-	os_command += '-pointsize 25 -fill white -annotate +10+1060 ' + repr(config['annotation_text']) + ' ' +  file_name
+	os_command = f'/usr/bin/convert {file_name} -pointsize 25 -fill white -annotate +{config["width"] - 240}+{config["height"] - 20} {repr(time)} '
+	os_command += f'-pointsize 25 -fill white -annotate +10+{config["height"] - 20} {repr(config["annotation_text"])} {file_name}'
 	create_log('Adding annotation to picture')
 	os.system(os_command)
 
-def run_loop(base, pause, config):
-	am = config['am']
-	pm = config['pm']
+def run_loop():
+	base = config["base_path"]
+	pause = config["delay"]
+	am = config["am"]
+	pm = config["pm"]
 
-	create_log('Shots will be taken between {} AM and {} PM'.format(am, pm))
+	create_log(f'Shots will be taken between {am} AM and {pm} PM')
 
 	while True:
 		time_now = int(time.strftime('%H%M'))
 
-		if time_now > am and pm > time_now:
+		if time_now > am and time_now < pm:
 			now = datetime.now()
 			path = prepare_dir(base, now)
 
-			name = str(time_now) + '.jpg'
-			create_log('Capturing {}'.format(name))
-			file_name = base + '/' + path + '/' + name
+			name = f'{time_now}.jpg'
+			create_log(f'Capturing {name}')
 
-			os_command = make_os_command(config, file_name)
+			file_name = f'{base}/{path}/{name}'
+			os_command = make_os_command(file_name)
 			os.system(os_command)
-			create_log('Written: {}'.format(file_name))
+			create_log(f'Written: {file_name}')
 
-			if config['enable_annotation']:
+			if config["enable_annotation"]:
 				annotate(file_name)
 		else:
-			create_log('Shot cancelled during hours of darkness time: {}'.format(time_now))
+			create_log(f'Shot cancelled during hours of darkness time: {time_now}')
 		
-		create_log('Pausing {} seconds'.format(pause))
+		create_log(f'Pausing {pause} seconds')
 		time.sleep(pause)
 
 if __name__ == '__main__':
 	try:
-		pause_interval = config['delay']
-		base_path = config['base_path']
-		run_loop(base_path, pause_interval, config)
+		run_loop()
 	except KeyboardInterrupt:
 		print('Cancelling take.py')
